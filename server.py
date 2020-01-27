3e3b6b464e4d01da5482a9179f3d235f59a5023c
Anupam-Panda <talk2anupamcontai@gmail.com>
2020-01-27 15:21:00 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    #input_length = len(name)
+    #reversed_name = str[input_length::-1]
+    #print ("reversed name {reversed_name}")
+    reversed_name = ''.join(reversed(name))
+    greeting = f"Hello {name}! Your name in reverse is {reversed_name}!!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
