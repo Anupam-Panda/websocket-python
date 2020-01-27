21abe0f3951f1a44a507e47baa1daaf98d60fba8
Anupam-Panda <talk2anupamcontai@gmail.com>
2020-01-27 18:04:30 +0530

    Update port

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "localhost", 8080)
+start_server = websockets.serve(hello, "localhost", 8083)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
